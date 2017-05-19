0ec1226ef3134b7e9371a34c1fcdba949ca730a1
Anja <anja.trinczek@stud.hs-kempten.de>
2017-05-19 23:17:29 +0200

    Basic Player Movement Script

---
+++ Assets/Scripts/Components/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public string XAxisInputString = "Horizontal";
+	public string YAxisInputString = "Vertical";
+	public float movementspeed;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		movementspeed = 100.0f;
+		
+	}
+
+
+	virtual void FixedUpdate(){
+		
+		float moveHorizontal = Input.GetAxis(XAxisInputString);
+		float moveVertical = Input.GetAxis (YAxisInputString);
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		rb2d.AddForce (movement*movementspeed);
+
+	}
+
