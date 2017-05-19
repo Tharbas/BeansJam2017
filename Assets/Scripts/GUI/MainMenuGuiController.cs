5ea0f310e185a8c0b0de57ad4818e465f5a65f0a
Jan Böhme <celdrik@gmx.net>
2017-05-20 01:10:24 +0200

    First Mockup of main menu

---
+++ Assets/Scripts/GUI/MainMenuGuiController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuGuiController : MonoBehaviour
+{
+    public enum WindowTypes
+    {
+        ChooseControls,
+        Instructions,
+        MainMenu
+    }
+
+    [SerializeField]
+    private GameObject mainMenuWindow;
+
+    [SerializeField]
+    private GameObject chooseControlsWindow;
+
+    [SerializeField]
+    private GameObject instructionsWindow;
+
+
+    public void Start ()
+    {
+        this.HideAllWindows();
+        this.mainMenuWindow.SetActive(true);
+	}
+
+    private void HideAllWindows()
+    {
+        this.instructionsWindow.SetActive(false);
+        this.chooseControlsWindow.SetActive(false);
+    }
+
+    public void ShowWindow(WindowTypes type)
+    {
+        switch (type)
+        {
+            case WindowTypes.ChooseControls:
+                this.chooseControlsWindow.SetActive(true);
+                break;
+            case WindowTypes.Instructions:
+                this.instructionsWindow.SetActive(true);
+                break;
+            case WindowTypes.MainMenu:
+                this.mainMenuWindow.SetActive(true);
+                break;
+        }
+    }
+
+    public void HideWindow(WindowTypes type)
+    {
+        switch (type)
+        {
+            case WindowTypes.ChooseControls:
+                this.chooseControlsWindow.SetActive(false);
+                break;
+            case WindowTypes.Instructions:
+                this.instructionsWindow.SetActive(false);
+                break;
+            case WindowTypes.MainMenu:
+                this.mainMenuWindow.SetActive(false);
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/GUI/MainMenuGuiEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuGuiEventHandler : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        switch (eventData.pointerPress.gameObject.name)
+        {
+            case "Button_StartGame":
+                GuiController.Instance.HideWindow(MainMenuGuiController.WindowTypes.MainMenu);
+                GuiController.Instance.ShowWindow(MainMenuGuiController.WindowTypes.ChooseControls);
+                break;
+            case "Button_Instructions":
+                GuiController.Instance.HideWindow(MainMenuGuiController.WindowTypes.MainMenu);
+                GuiController.Instance.ShowWindow(MainMenuGuiController.WindowTypes.Instructions);
+                break;
+            case "Button_Instructions_Back":
+                GuiController.Instance.HideWindow(MainMenuGuiController.WindowTypes.Instructions);
+                GuiController.Instance.ShowWindow(MainMenuGuiController.WindowTypes.MainMenu);
+                break;
+            case "Button_ChooseControls_Back":
+                GuiController.Instance.HideWindow(MainMenuGuiController.WindowTypes.ChooseControls);
+                GuiController.Instance.ShowWindow(MainMenuGuiController.WindowTypes.MainMenu);
+                break;
+            case "Button_QuitGame":
+                Application.Quit();
+                break;
+        }
+    }
+}
