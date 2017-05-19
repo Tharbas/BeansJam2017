7f04f8bc129998c487dfe207341066a4d7701a21
Tharbas <tharbas@googlemail.com>
2017-05-20 00:42:18 +0200

    Layout & NavMesh Test 1
    Layout & NavMesh Test 1

--- Assets/Scripts/Entities/AIEntity.cs
+++ Assets/Scripts/Entities/AIEntity.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIEntity : MonoBehaviour {
 
-	// Use this for initialization
+
+    // Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
 }
--- Assets/Scripts/Systems/AISystem.cs
+++ Assets/Scripts/Systems/AISystem.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+public enum AIStates
+{
+    Idle,
+    WalkingToStore,
+    WaitingInLine,
+    RandomMovement
+}
 
 public class AISystem : MonoBehaviour {
 
+    private List<AIEntity> npcs;
+
 	// Use this for initialization
 	void Start () {
-		
+        npcs = new List<AIEntity>();
+        npcs.AddRange(Component.FindObjectsOfType<AIEntity>());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	    if (Input.GetMouseButtonUp(0))
+        {
+            npcs[0].GetComponent<NavMeshAgent>().Move(new Vector3(0.1f, 0, 0.1f));
+            
+        }
 	}
 }
