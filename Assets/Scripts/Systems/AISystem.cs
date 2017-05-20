c56040a31c78e47bcab668d713fe969047d806c0
Tharbas <tharbas@googlemail.com>
2017-05-20 03:34:38 +0200

    AI improvements
    ai is wandering around and waiting before stores

--- Assets/Scripts/Systems/AISystem.cs
+++ Assets/Scripts/Systems/AISystem.cs
@@ -15,21 +15,56 @@ public class AISystem : MonoBehaviour {
 
     private List<AIEntity> npcs;
 
+    private List<WaypointComponent> waypoints;
+
+
 	// Use this for initialization
 	void Start () {
         npcs = new List<AIEntity>();
         npcs.AddRange(Component.FindObjectsOfType<AIEntity>());
-	}
+
+        waypoints = new List<WaypointComponent>();
+        waypoints.AddRange(Component.FindObjectsOfType<WaypointComponent>());
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetMouseButtonUp(0))
+        foreach (AIEntity npc in npcs)
         {
-            Vector3 pos = Component.FindObjectOfType<WaypointComponent>().transform.position;
-            pos.y = 0;
-            foreach (AIEntity npc in npcs)
+            if(npc.CurrentSate == AIStates.WaitingInLine)
             {
-                npc.GetComponent<NavMeshAgent>().SetDestination(pos);
+                if (npc.IdleTime > 0f)
+                {
+                    npc.IdleTime -= Time.deltaTime;
+                    continue;
+                }
+                else
+                {
+                    npc.CurrentSate = AIStates.Idle;
+                }
+            }
+            NavMeshAgent agent = npc.GetComponent<NavMeshAgent>();
+            if (agent.path == null || agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (npc.CurrentSate == AIStates.WalkingToStore)
+                {
+                    npc.CurrentSate = AIStates.WaitingInLine;
+                    npc.IdleTime = Random.Range(npc.MinIdleTime, npc.MaxIdleTime);
+                    continue;
+                }
+
+                agent.ResetPath();
+                int rand = Random.Range(0, waypoints.Count);
+                WaypointComponent nextWaypoint = waypoints[rand];
+                while (npc.CurrentTarget != null && nextWaypoint == npc.CurrentTarget)
+                {
+                    rand = Random.Range(0, waypoints.Count);
+                    nextWaypoint = waypoints[rand];
+                }
+                npc.CurrentTarget = nextWaypoint;
+                npc.CurrentSate = AIStates.WalkingToStore;
+                npc.GetComponent<NavMeshAgent>().SetDestination(npc.CurrentTarget.transform.position);
             }
         }
 	}
